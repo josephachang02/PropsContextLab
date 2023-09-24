2c6265465f794488e788ae357120f46cc44ab6c7
Joe Chang <131396136+josephachang02@users.noreply.github.com>
2023-09-23 17:25:04 -0700

    sat

---
+++ PropsAndContextLab/src/pages/Cart/index.jsx
@@ -0,0 +1,16 @@
+const Cart = () => {
+    return (
+      <div>
+          {/* we need products array here */}
+          {/* {products.filter((product) => {
+              if (product.inCart) {
+                  return true
+              } else {
+                  return false
+              }
+          })} */}
+      </div>
+    )
+  }
+  
+  export default Cart
